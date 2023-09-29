9006a0057c01409afd50800b282b99e03aa5e0b2
nikhil730 <nikhilbhalla196@gmail.com>
2023-09-30 02:26:01 +0530

    dev branch setup done

--- client/src/axios/axios.js
+++ client/src/axios/axios.js
@@ -1,3 +1,4 @@
 import axios from "axios";
-const api = axios.create({ baseURL: "https://docfinder-backend.onrender.com" });
+// const api = axios.create({ baseURL: "https://docfinder-backend.onrender.com" });
+const api = axios;
 export default api;
